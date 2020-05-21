4c0d9c18bd39a02cfe068b6db47febcc6d891331
FUYUHSUAN <ramy1231863@gmail.com>
2020-05-22 00:14:19 +0800

    add

---
+++ Midterm/activity.js
@@ -0,0 +1,33 @@
+const page = {}
+page.answer_mood = `
+    <link rel="stylesheet" href="activity1.css"/>
+    <link rel="stylesheet" href="activity2.css"/>
+
+    <div id="rcorners3">
+        <h2  style="text-align: center">你的性情如何？</h2>
+        <p>假設獨居的你，居住在兩房一廳的屋子，你將如何佈置其中一間面對陽光的房間？</p>
+        <div id="rcorners4" class="answer">
+            <h3>A. 放張床/當寢室</h3>
+            <p>你是一個非常冷靜的人，對於任何的事都有極合理的看法，就算是處在熱戀的的階段也會仔細地考慮對方是否合適。</p>
+            <p>若是遇到一個理想的物件，一定會積極主動地去追求，但是由於你的眼光太高，所以經由相親而結婚的幾率很高。</p>
+        </div>
+
+        <div id="rcorners4" class="answer">
+            <h3>B. 擺張書桌、書架/當書房</h3>
+            <p>你的好奇心很強，東想西想的，所以你對結婚並不積極。</p>
+            <p>你熱衷於生活中的一切，卻少有戀愛的經驗，但你可能會突然地閃電結婚，嚇死周遭的人。</p>
+        </div>
+
+        <div id="rcorners4" class="answer">
+            <h3>C. 當會客室</h3>
+            <p>你是一個很喜歡熱鬧的人，身邊的男女朋友有一堆，是一個機會很多的人。</p>
+            <p>因此你可能會在一場戀愛之後來一個閃電結婚，或總是朋友，而無法發展戀愛關係。</p>
+        </div>
+    
+        <div id="rcorners4" class="answer">
+            <h3>D. 擺餐桌/當餐廳</h3>
+            <p>對於婚姻你懷有很大的夢想，你渴望擁有一場轟轟烈烈的戀愛，豪華的婚禮，從此過著幸福快樂的日子。</p>
+            <p>但事與願違，婚期總是愈拖愈晚。婚後的你是以家庭為優先的人。</p>
+        </div>
+    </div>
+`
---
+++ Midterm/test.js
@@ -0,0 +1,8 @@
+/*測攔*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
