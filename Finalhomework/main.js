6d0336acdb0568d190c75b081e480878e7d6e48c
FUYUHSUAN <ramy1231863@gmail.com>
2020-06-07 11:55:15 +0800

    add

---
+++ Finalhomework/main.js
@@ -0,0 +1,18 @@
+/*測攔*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+   
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
